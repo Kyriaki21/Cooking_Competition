dff1cbc51cac018d4aa7457a2583052e14c61241
Kyriaki21 <118129365+Kyriaki21@users.noreply.github.com>
2024-04-15 20:05:22 +0300

    Add files via upload

--- ddl preproduction.sql
+++ ddl preproduction.sql
@@ -2,44 +2,74 @@ create database Cooking_Competition;
 
 use Cooking_Competition;
 
+CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Cuisine` (
+  `idCuisine` INT NOT NULL,
+  `Cuisine` VARCHAR(45) NULL,
+  PRIMARY KEY (`idCuisine`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Type_Meal` (
+  `idType_Meal` INT NOT NULL,
+  `Type_Meal` VARCHAR(45) NULL,
+  PRIMARY KEY (`idType_Meal`))
+ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Food_Group` (
   `idFood_Group` INT NOT NULL,
   `name_food_group` VARCHAR(45) NOT NULL,
-  `description_food_group` VARCHAR(45) NOT NULL,
+  `description_food_group` TEXT NOT NULL,
   `Recipe_Category` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idFood_Group`),
-  UNIQUE INDEX `name_food_group_UNIQUE` (`name_food_group` ASC) VISIBLE)
+  UNIQUE INDEX `name_food_group_UNIQUE` (`name_food_group` ASC) VISIBLE,
+  UNIQUE INDEX `Recipe_Category_UNIQUE` (`Recipe_Category` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Equipment` (
   `idEquipment` INT NOT NULL,
   `equip_name` VARCHAR(45) NOT NULL,
-  `equip_use` VARCHAR(200) NOT NULL,
+  `equip_use` TEXT NOT NULL,
   PRIMARY KEY (`idEquipment`),
-  UNIQUE INDEX `equip_name_UNIQUE` (`equip_name` ASC) VISIBLE,
-  UNIQUE INDEX `equip_use_UNIQUE` (`equip_use` ASC) VISIBLE)
+  UNIQUE INDEX `equip_name_UNIQUE` (`equip_name` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe` (
   `idRecipe` INT NOT NULL,
   `title` VARCHAR(200) NOT NULL,
   `syntagi` VARCHAR(45) NOT NULL,
-  `description` TEXT(500) NOT NULL,
+  `description` TEXT NOT NULL,
   `difficulty` TINYINT NOT NULL,
   `prep_time` TINYINT NOT NULL,
   `cook_time` TINYINT NOT NULL,
   `portions` TINYINT NOT NULL,
-  `Cuisine` VARCHAR(45) NOT NULL,
-  `Type_meal` VARCHAR(45) NOT NULL,
+  `Cuisine_id` INT NOT NULL,
+  `Type_Meal_id` INT NOT NULL,
+  `Steps_text` TEXT NOT NULL,
   PRIMARY KEY (`idRecipe`),
-  CONSTRAINT range_dif check (difficulty IN (1,5)))
+  INDEX `fk_Recipe_Cuisine1_idx` (`Cuisine_id` ASC) VISIBLE,
+  INDEX `fk_Recipe_Type_Meal1_idx` (`Type_Meal_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Recipe_Cuisine1`
+    FOREIGN KEY (`Cuisine_id`)
+    REFERENCES `Cooking_Competition`.`Cuisine` (`idCuisine`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Recipe_Type_Meal1`
+    FOREIGN KEY (`Type_Meal_id`)
+    REFERENCES `Cooking_Competition`.`Type_Meal` (`idType_Meal`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT range_dif check (difficulty IN (1,5)),
+  CONSTRAINT syntagi_check check (syntagi IN ('Cooking','Pastry')),
+  CONSTRAINT prep_time_check check (prep_time > 0),
+  CONSTRAINT portions_check check (portions > 0),
+  CONSTRAINT cook_time_check check (cook_time >= 0))
 ENGINE = InnoDB
 COMMENT = '\n';
 
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Label` (
   `idLabel` INT NOT NULL,
   `Label_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`idLabel`))
+  PRIMARY KEY (`idLabel`),
+  UNIQUE INDEX  `Label_name_UNIQUE` (`Label_name` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Label` (
@@ -64,7 +94,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Steps` (
   `idSteps` INT NOT NULL,
   `Step_number` TINYINT NOT NULL,
-  `Step_description` VARCHAR(45) NOT NULL,
+  `Step_description` TEXT NOT NULL,
   `Recipe_idRecipe` INT NOT NULL,
   PRIMARY KEY (`idSteps`),
   INDEX `fk_Steps_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
@@ -111,19 +141,25 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Ingredients` (
     FOREIGN KEY (`Food_Group_idFood_Group`)
     REFERENCES `Cooking_Competition`.`Food_Group` (`idFood_Group`)
     ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON UPDATE CASCADE,
+    CONSTRAINT fat_pos_constraint CHECK (COALESCE(fat, 0) >= 0),
+    CONSTRAINT protein_pos_constraint CHECK (COALESCE(protein, 0) >= 0),
+    CONSTRAINT carbohydrate_pos_constraint CHECK (COALESCE(carbohydrate, 0) >= 0),
+    CONSTRAINT calories_pos_constraint CHECK (COALESCE(calories, 0) >= 0),
+    UNIQUE INDEX  `Ingredient_name_UNIQUE` (`Ingredient_name` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Ingredients` (
   `Recipe_idRecipe` INT NOT NULL,
   `Ingredients_idIngredients` INT NOT NULL,
-  `Quantity` SMALLINT NULL,
+  `Quantity` SMALLINT NOT NULL,
   `is_basic` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`Recipe_idRecipe`, `Ingredients_idIngredients`),
   INDEX `fk_Recipe_has_Ingredients_Ingredients1_idx` (`Ingredients_idIngredients` ASC) VISIBLE,
   INDEX `fk_Recipe_has_Ingredients_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
   CONSTRAINT unique_rec_label UNIQUE (Recipe_idRecipe,Ingredients_idIngredients),
   CONSTRAINT chk_basic CHECK (is_basic IN (0, 1)),
+  CONSTRAINT quantity_pos CHECK (Quantity > 0),
   CONSTRAINT `fk_Recipe_has_Ingredients_Recipe1`
     FOREIGN KEY (`Recipe_idRecipe`)
     REFERENCES `Cooking_Competition`.`Recipe` (`idRecipe`)
