508f03a26ac5043d784fe97b5d366c95cec86129
Kyriaki21 <118129365+Kyriaki21@users.noreply.github.com>
2024-04-27 16:02:58 +0300

    Add files via upload

--- ddl preproduction.sql
+++ ddl preproduction.sql
@@ -1,49 +1,55 @@
-create database Cooking_Competition;
 
-use Cooking_Competition;
+DROP SCHEMA IF EXISTS Cooking_Competition;
+CREATE SCHEMA Cooking_Competition;
+USE Cooking_Competition;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Cuisine` (
-  `idCuisine` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Cuisine` (
+  `idCuisine` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Cuisine` VARCHAR(45) NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idCuisine`))
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Type_Meal` (
-  `idType_Meal` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Type_Meal` (
+  `idType_Meal` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Type_Meal` VARCHAR(45) NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idType_Meal`))
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Food_Group` (
-  `idFood_Group` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Food_Group` (
+  `idFood_Group` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name_food_group` VARCHAR(45) NOT NULL,
   `description_food_group` TEXT NOT NULL,
   `Recipe_Category` VARCHAR(45) NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idFood_Group`),
   UNIQUE INDEX `name_food_group_UNIQUE` (`name_food_group` ASC) VISIBLE,
   UNIQUE INDEX `Recipe_Category_UNIQUE` (`Recipe_Category` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Equipment` (
-  `idEquipment` INT NOT NULL,
+CREATE TABLE  `Cooking_Competition`.`Equipment` (
+  `idEquipment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `equip_name` VARCHAR(45) NOT NULL,
   `equip_use` TEXT NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idEquipment`),
   UNIQUE INDEX `equip_name_UNIQUE` (`equip_name` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe` (
-  `idRecipe` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Recipe` (
+  `idRecipe` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
-  `syntagi` VARCHAR(45) NOT NULL,
   `description` TEXT NOT NULL,
   `difficulty` TINYINT NOT NULL,
   `prep_time` TINYINT NOT NULL,
   `cook_time` TINYINT NOT NULL,
   `portions` TINYINT NOT NULL,
-  `Cuisine_id` INT NOT NULL,
-  `Type_Meal_id` INT NOT NULL,
+  `Cuisine_id` INT UNSIGNED NOT NULL,
+  `Type_Meal_id` INT UNSIGNED NOT NULL,
   `Steps_text` TEXT NOT NULL,
+  `syntagi` ENUM('cooking', 'pastry') NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idRecipe`),
   INDEX `fk_Recipe_Cuisine1_idx` (`Cuisine_id` ASC) VISIBLE,
   INDEX `fk_Recipe_Type_Meal1_idx` (`Type_Meal_id` ASC) VISIBLE,
@@ -58,23 +64,51 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe` (
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT range_dif check (difficulty IN (1,5)),
-  CONSTRAINT syntagi_check check (syntagi IN ('Cooking','Pastry')),
   CONSTRAINT prep_time_check check (prep_time > 0),
   CONSTRAINT portions_check check (portions > 0),
   CONSTRAINT cook_time_check check (cook_time >= 0))
-ENGINE = InnoDB
-COMMENT = '\n';
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Label` (
-  `idLabel` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Concept` (
+  `idConcept` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Concept_name` VARCHAR(255) NOT NULL,
+  `Concept_description` TEXT DEFAULT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idConcept`))
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE  `Cooking_Competition`.`Recipe_has_Concept` (
+  `Recipe_idRecipe` INT UNSIGNED NOT NULL,
+  `Concept_idConcept` INT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Recipe_idRecipe`, `Concept_idConcept`),
+  INDEX `fk_Recipe_has_Concept_Concept1_idx` (`Concept_idConcept` ASC) VISIBLE,
+  INDEX `fk_Recipe_has_Concept_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
+  CONSTRAINT unique_rec_concept UNIQUE (Recipe_idRecipe,Concept_idConcept),
+  CONSTRAINT `fk_Recipe_has_Concept_Recipe1`
+    FOREIGN KEY (`Recipe_idRecipe`)
+    REFERENCES `Cooking_Competition`.`Recipe` (`idRecipe`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Recipe_has_Concept_Concept1`
+    FOREIGN KEY (`Concept_idConcept`)
+    REFERENCES `Cooking_Competition`.`Concept` (`idConcept`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `Cooking_Competition`.`Label` (
+  `idLabel` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Label_name` VARCHAR(45) NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idLabel`),
   UNIQUE INDEX  `Label_name_UNIQUE` (`Label_name` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Label` (
-  `Recipe_idRecipe` INT NOT NULL,
-  `Label_idLabel` INT NOT NULL,
+CREATE TABLE  `Cooking_Competition`.`Recipe_has_Label` (
+  `Recipe_idRecipe` INT UNSIGNED NOT NULL,
+  `Label_idLabel` INT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Recipe_idRecipe`, `Label_idLabel`),
   INDEX `fk_Recipe_has_Label_Label1_idx` (`Label_idLabel` ASC) VISIBLE,
   INDEX `fk_Recipe_has_Label_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
@@ -89,13 +123,14 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Label` (
     REFERENCES `Cooking_Competition`.`Label` (`idLabel`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Steps` (
-  `idSteps` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Steps` (
+  `idSteps` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Step_number` TINYINT NOT NULL,
   `Step_description` TEXT NOT NULL,
-  `Recipe_idRecipe` INT NOT NULL,
+  `Recipe_idRecipe` INT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idSteps`),
   INDEX `fk_Steps_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
   CONSTRAINT Unique_rec_step UNIQUE	(Recipe_idRecipe,idSteps),
@@ -104,11 +139,12 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Steps` (
     REFERENCES `Cooking_Competition`.`Recipe` (`idRecipe`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Equipment` (
-  `Recipe_idRecipe` INT NOT NULL,
-  `Equipment_idEquipment` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Recipe_has_Equipment` (
+  `Recipe_idRecipe` INT UNSIGNED NOT NULL,
+  `Equipment_idEquipment` INT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Recipe_idRecipe`, `Equipment_idEquipment`),
   INDEX `fk_Recipe_has_Equipment_Equipment1_idx` (`Equipment_idEquipment` ASC) VISIBLE,
   INDEX `fk_Recipe_has_Equipment_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
@@ -123,18 +159,19 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Equipment` (
     REFERENCES `Cooking_Competition`.`Equipment` (`idEquipment`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Ingredients` (
-  `idIngredients` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Ingredients` (
+  `idIngredients` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Ingredient_name` VARCHAR(45) NOT NULL,
   `fat` SMALLINT NULL,
   `protein` SMALLINT NULL,
   `carbohydrate` SMALLINT NULL,
   `calories` SMALLINT NULL,
-  `Food_Group_idFood_Group` INT NOT NULL,
+  `Food_Group_idFood_Group` INT UNSIGNED NOT NULL,
   `Measurement_Type` VARCHAR(45) NOT NULL,
   `Default_scale` TINYINT NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idIngredients`),
   INDEX `fk_Ingredients_Food_Group_idx` (`Food_Group_idFood_Group` ASC) VISIBLE,
   CONSTRAINT `fk_Ingredients_Food_Group`
@@ -147,13 +184,14 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Ingredients` (
     CONSTRAINT carbohydrate_pos_constraint CHECK (COALESCE(carbohydrate, 0) >= 0),
     CONSTRAINT calories_pos_constraint CHECK (COALESCE(calories, 0) >= 0),
     UNIQUE INDEX  `Ingredient_name_UNIQUE` (`Ingredient_name` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Ingredients` (
-  `Recipe_idRecipe` INT NOT NULL,
-  `Ingredients_idIngredients` INT NOT NULL,
+CREATE TABLE `Cooking_Competition`.`Recipe_has_Ingredients` (
+  `Recipe_idRecipe` INT UNSIGNED NOT NULL,
+  `Ingredients_idIngredients` INT UNSIGNED NOT NULL,
   `Quantity` SMALLINT NOT NULL,
   `is_basic` TINYINT NOT NULL DEFAULT 0,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Recipe_idRecipe`, `Ingredients_idIngredients`),
   INDEX `fk_Recipe_has_Ingredients_Ingredients1_idx` (`Ingredients_idIngredients` ASC) VISIBLE,
   INDEX `fk_Recipe_has_Ingredients_Recipe1_idx` (`Recipe_idRecipe` ASC) VISIBLE,
@@ -170,4 +208,37 @@ CREATE TABLE IF NOT EXISTS `Cooking_Competition`.`Recipe_has_Ingredients` (
     REFERENCES `Cooking_Competition`.`Ingredients` (`idIngredients`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `Cooking_Competition`.`Cook` (
+  `idCook` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `phone_number` VARCHAR(15) NOT NULL,
+  `birth_date` DATE NOT NULL,
+  `age` INT NOT NULL,
+  `Years_experience` INT NOT NULL,
+  `Status` ENUM('C cook', 'B cook', 'A cook', 'assistant head Chef', 'Chef') NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idCook`))
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE  `Cooking_Competition`.`Cook_Cuisine` (
+  `Cook_idCook` INT UNSIGNED NOT NULL,
+  `Cuisine_idCuisine` INT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Cook_idCook`, `Cuisine_idCuisine`),
+  INDEX `fk_Cook_Cuisine_Cuisine1_idx` (`Cuisine_idCuisine` ASC) VISIBLE,
+  INDEX `fk_Cook_Cuisine_Cook1_idx` (`Cook_idCook` ASC) VISIBLE,
+  CONSTRAINT unique_rec_concept UNIQUE (Cook_idCook,Cuisine_idCuisine),
+  CONSTRAINT `fk_Cook_Cuisine_Cook1`
+    FOREIGN KEY (`Cook_idCook`)
+    REFERENCES `Cooking_Competition`.`Cook` (`idCook`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Cook_Cuisine_Cuisine1`
+    FOREIGN KEY (`Cuisine_idCuisine`)
+    REFERENCES `Cooking_Competition`.`Cuisine` (`idCuisine`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
